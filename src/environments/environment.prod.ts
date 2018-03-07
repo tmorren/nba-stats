4e79d48a3f6544afd922f26784220fd6ee2ac100
Tyler <tmorren1@gmail.com>
2018-03-06 20:35:46 -0500

    Prod variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUsername: 'tmorren',
+  apiPassword: '80GFTJ3iOo7s',
+  baseUrl: 'https://api.mysportsfeeds.com/v1.1/pull/nba'
 };
